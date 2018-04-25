fcd0728bf8596aae83482e1337a8ef7eb2932897
LiliyaStefanova <liliya.stefanova@gmail.com>
2018-04-25 22:02:23 +0100

    Separated list and search into separate component classes

--- src/App.js
+++ src/App.js
@@ -52,28 +52,55 @@ class App extends Component {
     }
 
   render() {
-
+        const {searchTerm, list} = this.state;
     return (
       <div className="App">
-          <form>
-              <input type="text" onChange={this.onSearchChange}/>
-          </form>
-          {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-                  <div key = {item.objectID}>
-                      <span>
-                          <a href = {item.url}>{item.title}</a>
-                      </span>
-                      <span>{item.author}</span>
-                      <span>{item.num_comments}</span>
-                      <span>{item.points}</span>
-                      <button onClick={() => this.onDismiss(item.objectID)} type="button">
-                          Dismiss
-                      </button>
-                  </div>
-          )}
+          <Search
+          value={searchTerm}
+          onSearchChange={this.onSearchChange}/>
+          <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}/>
       </div>
     );
   }
+}
+
+class Search extends Component{
+    render(){
+        const {value, onSearchChange} = this.props;
+        return (
+            <form>
+                <input type="text"
+                       value={value}
+                       onChange={onSearchChange}/>
+            </form>
+        )
+    }
+}
+
+class Table extends Component{
+    render(){
+        const{list, pattern, onDismiss} = this.props;
+        return (
+            <div>
+                {list.filter(isSearched(pattern)).map(item =>
+                <div key = {item.objectID}>
+                      <span>
+                          <a href = {item.url}>{item.title}</a>
+                      </span>
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                    <button onClick={() => onDismiss(item.objectID)} type="button">
+                        Dismiss
+                    </button>
+                </div>
+                )}
+            </div>
+            );
+    }
 }
 
 export default App;
